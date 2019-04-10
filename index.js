4dbe54ff6e69ba36ac8b494d65d73b31093e3514
advyth <advythashok@gmail.com>
2019-04-10 13:21:27 +0530

    Trying to fix CORS

--- index.js
+++ index.js
@@ -69,7 +69,24 @@ app.post('/register', function(req,res){
     
   })
 });
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.post('/admin/add', function(req,res){
 	console.log("Add movie response recieved");
 	var moviename = req.body.moviename;
