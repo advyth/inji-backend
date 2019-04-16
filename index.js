abe75083a6facd027ca1bde2d5e23ea5dc1cf8b7
advyth <advythashok@gmail.com>
2019-04-16 22:00:28 +0530

    changed made

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var mysql = require('mysql'); //MySQL middleware
 var cors = require('cors'); //For handling CORS requests.
 var bodyParser = require('body-parser'); //Library for parsing POST requests.
 var unid = require('uniqid'); //Library for generating unique ID's.
+var sqlClean = require('sqlstring'); //Library for dealing with SQL injection.
+const bcrypt = require('bcrypt'); //Library for hashing
 
 
 //Create the connection
@@ -53,8 +55,7 @@ app.post('/login', function(req,res){
 	var email = req.body.email;
 	var password = req.body.password;
 	console.log("email "+ email + " password "+password);
-	//var login_query = 'select * from users';
-	var login_query = "select * from users where email='"+email+"' and password='"+password+"'";
+	var login_query = sqlClean.format("select * from users where email=? and password=?",[email,password]);
 	var auth = [];
 	mysqlPool.query(login_query, function(err, result){
 		if(result.length > 0)
@@ -77,8 +78,9 @@ app.post('/register', function(req,res){
   console.log("Register request recieved");
   var username = req.body.username;
   var password = req.body.password;
-  var email = req.body.email;
-  var new_user_query = "insert into users values('"+email+"','"+username+"','"+password+"')";
+	var email = req.body.email;
+	const saltRounds = 10;
+  var new_user_query = sqlClean.format("insert into users values(?,?,?)",[email, username, password]);
   var check_query = "select * from users where email='"+email+"'";
   mysqlPool.query(check_query, function(err, result){
     if(err) throw err;
@@ -99,13 +101,13 @@ app.post('/register', function(req,res){
 
 app.post('/admin/add', function(req,res){
 	console.log("Add movie response recieved");
-	var moviename = req.body.moviename;
-	var director = req.body.director;
-	var genre = req.body.genre;
-	var actor = req.body.actor;
-	var rating = req.body.rating;
-	var url = req.body.url;
-	var movie_add_query = "insert into movie_list values('"+moviename+"','"+director+"','"+genre+"','"+actor+"',"+rating+",'"+url+"',"+unid()+")";
+	var moviename = sqlClean.escape(req.body.moviename);
+	var director = sqlClean.escape(req.body.director);
+	var genre = sqlClean.escape(req.body.genre);
+	var actor = sqlClean.escape(req.body.actor);
+	var rating = sqlClean.escape(req.body.rating);
+	var url = sqlClean.escape(req.body.url);
+	var movie_add_query = sqlClean.format("insert into movie_list values(?,?,?,?,?,?,?)",[moviename, director, genre, actor, rating, url, unid()]);
 	mysqlPool.query(movie_add_query, function(err,result){
 		if(err) throw err;
 		res.send("Added 1 row");
@@ -114,7 +116,7 @@ app.post('/admin/add', function(req,res){
 
 app.post('/api/get/movies', function(req, res){
 	console.log("Send movie request recieved");
-	var auth = req.body.auth;
+	var auth = sqlClean.escape(req.body.auth);
 	if(auth)
 	{
 		var movie_get_query = "select * from movie_list";
@@ -127,10 +129,10 @@ app.post('/api/get/movies', function(req, res){
 });
 app.post('/api/get/single/movie', function(req, res){
 	console.log("Single movie request recieved");
-	var auth = req.body.auth;
+	var auth = sqlClean.escape(req.body.auth);
 	if(auth)
 	{	var id = req.body.id;
-		var single_movie_query = "Select * from movie_list where id='"+id+"'";
+		var single_movie_query = sqlClean.format("Select * from movie_list where id=?", id);
 		mysqlPool.query(single_movie_query, function(err, result){
 			if(err) throw err;
 			res.send(result);
@@ -140,21 +142,34 @@ app.post('/api/get/single/movie', function(req, res){
 });
 app.post('/api/add/review', function(req, res){
 	console.log("Review add request reciever");
-	var id = req.body.id;
-	var review = req.body.review;
-	var rating = req.body.rating;
-	var username = req.body.username;
-	var review_add_query = "insert into reviews values('"+username+"','"+review+"',"+rating+",'"+id+"')";
-	mysqlPool.query(review_add_query, function(err, result){
-		if(err) throw err;
-		res.send("Added");
+	var id = sqlClean.escape(req.body.id);
+	var review = sqlClean.escape(req.body.review);
+	var rating = sqlClean.escape(req.body.rating);
+	var username = sqlClean.escape(req.body.username);
+	var same_user_check_query = sqlClean.format("select * from reviews where username=?", username);
+	mysqlPool.query(same_user_check_query, function(err,result){
+		console.log(result.length);
+		console.log("Hit!");
+		if(result.length != 0)
+		{
+			res.send("user_duplicate");
+		}
+		else
+		{
+			var review_add_query = sqlClean.format("insert into reviews values(?,?,?,?)",[username,review, rating, id]);
+			mysqlPool.query(review_add_query, function(err, result){
+				if(err) throw err;
+				res.send("Added");
+			});
+		}
 	});
+	
 
 });
 
 app.post('/api/get/reviews', function(req, res){
 	var id = req.body.id;
-	var get_review_query = "select * from reviews where id='"+id+"'";
+	var get_review_query = sqlClean.format("select * from reviews where id=?",id);
 	mysqlPool.query(get_review_query, function(err, result){
 		if(err) throw err;
 		res.send(result);
@@ -162,9 +177,9 @@ app.post('/api/get/reviews', function(req, res){
 });
 
 app.post('/api/search/movie',function(req,res){
-	var movie = req.body.movie;
+	var movie = req.body.movie ;
 	console.log("Search request for "+ movie+" recieved");
-	var search_query = "select * from movie_list where name like '%"+movie+"%'";
+	var search_query = "select * from movie_list where name like '%+"+movie+"+%' ";
 	mysqlPool.query(search_query, function(err, result){
 		if(err) throw err;
 		if(result[0] == undefined)
@@ -193,7 +208,7 @@ app.post('/api/autocomplete/', function(req, res){
 
 
 
-app.listen(port, ()=> console.log("Listening on port 5000"));
+app.listen(port, ()=> console.log("Server has started and is ready for those requests :)"));
 
 //mysql://b2974b50757180:6545ca82@us-cdbr-iron-east-03.cleardb.net/heroku_90095f85482d913?reconnect=true
 //6545ca82
